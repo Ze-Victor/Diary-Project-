2f8699a93d0b258d6ee762bd6127dcf9b7f0c4fc
ze <josevictorjose8@gmail.com>
2020-06-22 16:05:20 -0300

    Modularização

--- include/time_date.h
+++ include/time_date.h
@@ -5,9 +5,9 @@
 
 struct Time_date
 {
-	unsigned short int dd, mm, aa;
-	unsigned short int hh, mm, ss;
-	
+	unsigned short int day, month, year;
+	unsigned short int hour, minute, second;
+
 	std::string format_current_date(const std::string &format);
 	std::string get_current_date();
 	std::string get_current_time();
